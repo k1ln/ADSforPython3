e3895226efa10bf97eabb94fbc3e85626a26c278
Kilian Hertel <k@1ln.de>
2022-04-29 15:58:13 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+r"""adsdb - pure Python Advantage Database interface.
+
+adsdb lets one access and manipulate Advantage databases
+in pure Python.
+
+http://code.google.com/p/adsdb
+
+----------------------------------------------------------------"""
+
+from distutils.core import setup
+
+setup(name='adsdb',
+      version='1.0.0',
+      description='pure Python Advantage database interface',
+      author='Peter Funk',
+      author_email='peter.funk@sybase.com',
+      url='http://code.google.com/p/adsdb',
+      py_modules=['adsdb'],
+     )
